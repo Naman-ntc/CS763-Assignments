fd1425cc2474d5a26fc546d7bbd28b52bd60bb76
Naman Jain <naman1205jain@gmail.com>
2018-01-17 08:07:42 +0530

    Added HW-1

---
+++ Assignment-1/HW1_CameraGeometry/Q6/code/myMainScript.m
@@ -0,0 +1,10 @@
+%% MyMainScript
+
+%tic;
+%% Your code here
+% thidgsgfjfgwfujwekw
+x_coor = (423*214 - 747*104)/110;
+factor = (747-x_coor)/(423-x_coor);
+p2_y = 583-factor*(150);
+h = (797-216)*180/(797-p2_y);
+%toc;
