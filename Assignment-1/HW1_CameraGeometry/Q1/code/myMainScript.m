9ad6952aa2f68d2b6bda065584e519267c69676e
Naman Jain <naman1205jain@gmail.com>
2018-01-18 18:53:14 +0530

    Problem-1 Complete

--- Assignment-1/HW1_CameraGeometry/Q1/code/myMainScript.m
+++ Assignment-1/HW1_CameraGeometry/Q1/code/myMainScript.m
@@ -14,29 +14,60 @@
 load('locations.mat')
 N = size(image_points);
 N = N(2);
+image_mean = mean(image_points,2);
+object_mean = mean(object_points,2);
+
+image_avg_dist = mean(sqrt(sum((image_points - repmat(image_mean, [1, N])).^2,1))); 
+object_avg_dist = mean(sqrt(sum((object_points - repmat(object_mean, [1, N])).^2,1)));
+
+image_scale = sqrt(2)/image_avg_dist;
+object_scale = sqrt(3)/object_avg_dist;
+
+T = image_scale*[eye(2) , -1*image_mean];
+T = [T; [zeros(1,2) , 1]];
+
+U = object_scale*[eye(3) , -1*object_mean];
+U = [U; [zeros(1,3) , 1]];
+
 image_points = [image_points; ones(1,N)];
 object_points = [object_points; ones(1,N)];
-% XXXXXXXXXXXXXXXXXXX     TRANSFORM THE COORDINATES LEFT
+
+image_points = T * image_points;
+object_points = U * object_points;
+
 A = zeros(2*N,12);
 for i = 1:N
     A(2*i-1,1:4) = 0;
-    A(2*i-1,5:8) = -1*image_points(3,i)*object_points(:,i);
+    A(2*i-1,5:8) = -1*object_points(:,i);
     A(2*i-1,9:12) = image_points(2,i)*object_points(:,i);
-    A(2*i,1:4) = image_points(3,i)*object_points(:,i);
+    A(2*i,1:4) = -1*object_points(:,i);
     A(2*i,5:8) = 0;
-    A(2*i,9:12) = -1*image_points(1,i)*object_points(:,i);
+    A(2*i,9:12) = image_points(1,i)*object_points(:,i);
 end
 
 [~,~,V] = svd(A);
-P = V(:,12); % |P| not exactly 1 but close
-P = reshape(P,[3,4]); 
+P = V(:,12);
+P = reshape(P,[4,3])'; 
 
-[~,~,V] = svd(P); % Can also be computed algebraically actually (Ziesermann pg150)
-X0 = V(:,4); % Check -- Last homogeneous coordinated almost 1 but not exactly
-X0 = X0(1:3); 
+%P = inv(T)*P*U;
 
-% P = [M | -MC]
+%[~,~,V] = svd(P); % Can also be computed algebraically actually (Ziesermann pg150)
+%X0 = V(:,4); % Check -- Last homogeneous coordinated almost 1 but not exactly
+%X0 = X0(1:3); 
+
+% P = [M | -M X_0]
 
 M = P(1:3,1:3);
-[R,M] = qr(M);
-% R is the rotation matrix and M is a upper triangular matrix
+[R,K] = qr(M);
+% R is the rotation matrix and K is a upper triangular matrix
+
+X_0 = -1*inv(M)*P;
+
+%%%%%%%%%%
+%predicted_points = P*object_points;
+%predicted_points(1,:) = predicted_points(1,:)./(predicted_points(3,:));
+%predicted_points(2,:) = predicted_points(2,:)./(predicted_points(3,:));
+%predicted_points(3,:) = predicted_points(3,:)./(predicted_points(3,:));
+
+%norm(predicted_points-image_points)
+%%%%%%%%%%
--- Assignment-1/HW1_CameraGeometry/Q2/code/myMainScript.m
+++ Assignment-1/HW1_CameraGeometry/Q2/code/myMainScript.m
@@ -10,15 +10,15 @@ half_X = X/2;
 
 A = zeros(size(image));
 
-for i = 1:X
-    for j = 1:Z
+for i = 1:3:X
+    for j = 1:3:Y
         x = (i - half_X);
         y = (j - half_Y);
-        x = x/(half_X); y = y/(half_Y);
+        x = x/(5*half_X); y = y/(5*half_Y);
         r = sqrt(x^2 + y^2);
         q =  1 + q1*r + q2*r*r;
         src_x = x/q;
         src_y = y/q; 
-        A(i,j) = interp2(image,half_X*(1+src_x),half_Y*(1+src_y));
+        A(i,j) = interp2(image,5*half_X*(1+src_x),5*half_Y*(1+src_y));
     end
 end
