1d07a777427dc38170c17a40e907ee2d794eea6f
Sahil Shah <sahilshah00199@gmail.com>
2018-01-26 01:11:54 +0530

    A

--- Assignment-1/HW1_CameraGeometry/Q2/code/myMainScript.m
+++ Assignment-1/HW1_CameraGeometry/Q2/code/myMainScript.m
@@ -2,7 +2,7 @@
 
 tic;
 %% Your code here
-Img = im2double(imread('rad_checkerbox.jpg'));
+Img = im2double(imread('../input/rad_checkerbox.jpg'));
 Img = rgb2gray(Img);
 q2 = 0.5;
 q1 = 1;
@@ -20,26 +20,30 @@ for xd = 1:sz(1)
         norml_Xd = [(xd-cx)/cx, (yd-cy)/cy, 1]';
         xu = (xd-cx)/cx;
         yu = (yd-cy)/cy;
-        for i =1:10
+        xPrev = 10000;
+        yPrev = 10000;
+        while norm([xu-xPrev, yu-yPrev]) > 0.001 %run until convergence
+        %for i =1:4 %works in 4 iterations as well
+            xPrev = xu;
+            yPrev = yu;
             %recentering
-            H_mat = eye(3);
+            H_mat = eye(3); %The matrix we have to multiply x_d with it to get the next guess for x_u
             r_vec = [xu, yu];
-            r = norm(r_vec);
+            r = norm(r_vec); % calculate r using the latest guesses for x_u
             q_fac = 1 + (q1*r)+(q2*(r^2));
-            q_fac = 1/q_fac - 1;
-            %q_ = 1/((1/q_fac) -1);
-            H_mat(1,3) = norml_Xd(1)*q_fac;
-            H_mat(2,3) = norml_Xd(2)*q_fac;
+            f = 1/q_fac - 1; %Delta(x) = x_d*f and Delta(y) = y_d*f
+            H_mat(1,3) = norml_Xd(1)*f;
+            H_mat(2,3) = norml_Xd(2)*f; % complete the matrix that is to be multiplied
             %H_inv_mat = inv(H_mat);
-            T = H_mat*norml_Xd;
+            T = H_mat*norml_Xd; % T is our guess for x_u in the homogenous coods with third entry 1
             xu = T(1);
-            yu = T(2);
-            %xu, yu
-        end
-        A(max(1,min(round(xu*cx+cx),sz(1))), max(1,min(round(yu*cy+cy), sz(2)))) = Img(xd, yd);
+            yu = T(2); %Take first two values of T and assign to x_u
+             end
+        
+        A(max(1,min(round(xu*cx+cx),sz(1))), max(1,min(round(yu*cy+cy), sz(2)))) = Img(xd, yd); %Assign to the nearest inegral index
     end
 end
- imshow(A, [])
+ imshow(A, []) %display the undistorted image
 
 % CODE TO REGENERATE THE DISTORTED IMAGE USING THE GIVEN TRANSFORMATION 
 % 
