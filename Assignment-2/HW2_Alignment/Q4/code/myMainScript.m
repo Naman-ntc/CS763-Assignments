7f8232353b9c68f94c5f65eb974aa55ef7af9825
Naman Jain <naman1205jain@gmail.com>
2018-01-30 21:38:56 +0530

    reshaped assignment folders, used ransac to compute homography (Q2)

---
+++ Assignment-2/HW2_Alignment/Q4/code/myMainScript.m
@@ -0,0 +1,2 @@
+[a,b] = getFeaturePoints('../input/hill/1.JPG','../input/hill/2.JPG');
+ransacHomography(a,b,5);
--- Assignment-2/Q4/code/ransacHomography.m
+++
@@ -1,7 +0,0 @@
-function [ H ] = ransacHomography( x1, x2, thresh )
-%RANSACHOMOGRAPHY Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
