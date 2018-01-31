031f4a4f53878b6d4521a7131d95f2ab6d6ce8db
Sahil Shah <sahilshah00199@gmail.com>
2018-02-01 01:09:38 +0530

    q3 code done

--- Assignment-2/HW2_Alignment/Q3/code/jointEntropy.m
+++ Assignment-2/HW2_Alignment/Q3/code/jointEntropy.m
@@ -1,20 +1,21 @@
-load('Q3Data');
+function [entropy] = jointEntropy(movingImage, originalImage)
+
+    [h, w] = size(movingImage);
+    movingImage = floor(movingImage/10) + 1;
+    originalImage = floor(originalImage/10) + 1;
+    movingImage = reshape(movingImage, h*w, 1);
+    originalImage = reshape(originalImage, h*w, 1);
+    indices = [movingImage, originalImage];
+
+    histo = zeros(26, 26);
+
+    for i=1:size(indices, 1)
+        histo(indices(i, 1), indices(i, 2)) = histo(indices(i, 1), indices(i, 2)) + 1; 
+    end
+
+    remove0s = 1 - (histo > 0);
+    histo  = histo/(h*w);
+    entropy = -sum(sum( histo.*log(histo + remove0s) ));
 
 
-%%downsample for now
-movingImage = movingImage1(1:4:end, 1:4:end);
-originalImage = originalImage1(1:4:end, 1:4:end);
-
-[h, w] = size(movingImage);
-movingImage = floor(movingImage/10) + 1;
-originalImage = floor(originalImage/10) + 1;
-movingImage = reshape(movingImage, h*w, 1);
-originalImage = reshape(originalImage, h*w, 1);
-indices = [movingImage, originalImage];
-
-histo = zeros(26, 26);
-%%vectorise this part!
-
-for i=1:size(indices, 1)
-   histo(indices(i, 1), indices(i, 2)) = histo(indices(i, 1), indices(i, 2)) + 1; 
 end
--- Assignment-2/HW2_Alignment/Q3/code/myMainScript.m
+++ Assignment-2/HW2_Alignment/Q3/code/myMainScript.m
@@ -4,8 +4,8 @@ tic;
 %% Your code here
 
 %{
-movingImage1 = rotateAndTranslate('../input/negative_barbara.png');
-movingImage2 = rotateAndTranslate('../input/noflash1.jpg');
+movingImage1 = rotateAndTranslate(imread('../input/negative_barbara.png'), 23.5, -3);
+movingImage2 = rotateAndTranslate(imread('../input/noflash1.jpg'), 23.5, -3);
 save('Q3Data', movingImage1, movingImage2);
 movingImage1 = addNoise(movingImage1);
 movingImage2 = addNoise(movingImage2);
@@ -21,5 +21,14 @@ load('Q3Data');
 %%downsample for now
 movingImage = movingImage1(1:4:end, 1:4:end);
 originalImage = originalImage1(1:4:end, 1:4:end);
+entropyGrid1 = zeros(121, 24);
+
+for i=33:42
+    for j=13:17
+        currentImage = rotateAndTranslate(movingImage, i - 61, j - 13);
+        entropyGrid1(i, j) = jointEntropy(currentImage, originalImage);
+        i, j
+    end
+end
 
 toc;
