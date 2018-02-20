fe503faf09062e150f43d2de01495bb2da61fe0c
Sahil Shah <sahilshah00199@gmail.com>
2018-02-20 09:58:54 +0530

    small changes

--- Assignment-3/CS763DeepLearningHW/sahil/src/batchNorm.py
+++ Assignment-3/CS763DeepLearningHW/sahil/src/batchNorm.py
@@ -24,13 +24,13 @@ class BatchNorm(object):
 		#if (mode=='train'):
 		#elif (mode=='test'):
 		#return (self.weight*self.input_hat + self.bias)
-		return (self.weight * (input - self.running_mean) / torch.sqrt(self.running_var + 1e-6) + self.bias)
+		return (self.weight * (input - self.running_mean) / torch.sqrt(self.running_var + 1e-100) + self.bias)
 		#else :
 		#return "No such mode :/ "	
 	def backward(self,input,gradOutput):
 		self.sample_mean = input.mean(dim=0)
 		self.sample_var = input.var(dim=0,unbiased=False)
-		input_hat = (input - self.sample_mean) / torch.sqrt(self.sample_var + 1e-6)		
+		input_hat = (input - self.sample_mean) / torch.sqrt(self.sample_var + 1e-100)		
 		self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * self.sample_mean
 		self.running_var = self.momentum * self.running_var + (1 - self.momentum) * self.sample_var
 		self.input_hat = input_hat
@@ -38,12 +38,12 @@ class BatchNorm(object):
 		N = input.shape[0]
 		self.gradBias = gradOutput.sum(dim=0)
 		self.gradWeight = torch.sum(gradOutput * self.input_hat,dim=0)
-		gradVar = (gradOutput*self.weight*(input-self.sample_mean)*-0.5*torch.pow(self.sample_var + 1e-6,-1.5)).sum(dim=0)
-		gradMean1 = -1*(gradOutput*self.weight/torch.sqrt(self.sample_var + 1e-6)).sum(dim=0)
+		gradVar = (gradOutput*self.weight*(input-self.sample_mean)*-0.5*torch.pow(self.sample_var + 1e-100,-1.5)).sum(dim=0)
+		gradMean1 = -1*(gradOutput*self.weight/torch.sqrt(self.sample_var + 1e-100)).sum(dim=0)
 		gradMean2 = gradVar * (-2 * (input-self.sample_mean)).sum(dim=0)
 		gradMean = gradMean1 + gradMean2
 		#print(gradMean.type())
-		gradInput1 = (gradOutput*self.weight/torch.sqrt(self.sample_var + 1e-6))
+		gradInput1 = (gradOutput*self.weight/torch.sqrt(self.sample_var + 1e-100))
 		gradInput2 = (torch.ones(input.size())).type(torch.DoubleTensor) * gradMean 
 		gradInput2 = gradInput2/N  
 		gradInput3 = (input-self.sample_mean) * gradVar * 2./N
