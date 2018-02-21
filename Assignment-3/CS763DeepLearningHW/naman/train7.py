a03dcf59365608b210bf7f8352cb6feb2f748492
naman_ntc <naman1205jain@gmail.com>
2018-02-21 12:50:48 +0000

    Trained a model

--- Assignment-3/CS763DeepLearningHW/naman/train7.py
+++ Assignment-3/CS763DeepLearningHW/naman/train7.py
@@ -43,7 +43,7 @@ def train(model,lossClass,iterations, whenToPrint, batchSize, learningRate, par_
 				layer.bias -= learningRate*((1-momentum)*layer.gradBias + momentum*layer.momentumBias) + par_regularization*layer.bias
 				#layer.weight -= (learningRate*layer.gradWeight + par_regularization*layer.weight)
 				#layer.bias -= (learningRate*layer.gradBias + par_regularization*layer.bias)
-		plotIndex += 1
+		#plotIndex += 1
 
 
 def trainAcc(model):
