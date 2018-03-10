a2846d67056321d5e1811cd8f93ccb284866ed27
Naman Jain <naman1205jain@gmail.com>
2018-03-10 14:53:43 +0530

    Model done, to debug (hope not!!) and train

--- Assignment-4/CS763DeepLearningHW/Model.py
+++ Assignment-4/CS763DeepLearningHW/Model.py
@@ -1,5 +1,6 @@
 import torch
 from RNN import *
+from Linear import *
 
 class Model(object):
 	"""docstring for Model"""
@@ -10,13 +11,14 @@ class Model(object):
 		self.V = V
 		self.D = D
 		self.isTrain = isTrain
-		self.build()
-
-	def build(self):
-		self.cells = [None]*self.nLayers
-		for i in range(self.nLayers):
-			self.cells[i] = RNN(D,H)
+		self.RNN = RNN(D,H)
+		self.fc1 = Linear(H,2)
 
 	def forward(self,input):
-		for cell in self.cells:
-			
+		self.out1 = self.RNN.forward(input)
+		self.out2 = self.fc1.forward(out1)
+		return out2
+
+	def backward(self,input,gradOutput):
+		gradOut1 = self.fc1(self.out1,gradOutput)
+		return self.RNN(input,gradOut1)
