0cc0aca6af33e0631956b7fe16e90eccd35c19b7
Naman Jain <naman1205jain@gmail.com>
2018-03-10 13:55:50 +0530

    RNN forward probably done

--- Assignment-4/CS763DeepLearningHW/RNN.py
+++ Assignment-4/CS763DeepLearningHW/RNN.py
@@ -8,10 +8,23 @@ class RNN(object):
 		self.hidden_dim = hidden_dim
 		self.W = torch.randn((input_dim+hidden_dim),hidden_dim)
 		self.b = torch.randn(hidden_dim,1)
+		self.h0 = torch.zeros(hidden_dim)
 
 	def forward(self,input):
-		self.output = input.mm(W) + self.b
-	
+		"""
+		Takes a input a sequence of T vectors each of D dimension T x D
+		ncells equals number of timesteps we propogate
+		"""
+		ncells = input.size()[0]
+		self.hidden = torch.zeros(ncells+1,self.hidden_dim)
+		self.hidden[0,:] = self.h0
+		for i in range(ncells):
+			temp_input = torch.cat(input,hidden[i,:])
+			self.hidden[i+1,:] = temp_input.mm(self.W) + self.b
+			self.hidden[i+1,:] = torch.tanh(self.hidden[i+1,:])
+		self.h0 = hidden[ncells,:]
+		return self.h0
+
 	def backward(self,input,gradOutput):
 		self.gradB = gradOutput.sum(dim=0)
 		self.gradW = input.t().mm(gradOutput)
