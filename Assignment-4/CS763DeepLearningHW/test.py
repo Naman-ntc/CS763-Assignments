015e6ebe6eeb54d6619ffda02516676f4dbfec2b
Naman Jain <naman1205jain@gmail.com>
2018-03-13 18:19:25 +0530

    loss didnt really decrease, will check  backprop

--- Assignment-4/CS763DeepLearningHW/test.py
+++ Assignment-4/CS763DeepLearningHW/test.py
@@ -9,17 +9,40 @@ model = Model(-1,128,333,200,1)
 
 lossClass = Criterion()
 
-learningRate = 1e-1
+learningRate = 1e-5
 
-for i in range(10):
+count = 0
+
+for i in range(25):
 	trial_data = data[i].view(1,-1)
 	yPred = model.forward(trial_data)
-	lossGrad, loss = lossClass.backward(yPred, torch.DoubleTensor([labels[i]]))
-	if i%1 == 0:
-		print(i, loss)
-	model.clearGradParam()
-	model.backward(trial_data, lossGrad)
-	for layer in model.Layers:
-		if layer.isTrainable:
-			layer.weight -= learningRate*layer.gradWeight
-			layer.bias -= learningRate*layer.gradBias
+	count += (yPred.view(1,-1).max(dim=1)[1]==int(labels[i]))
+print(count)
+
+for kkk in range(200):
+	iterator = np.arange(25)
+	np.random.shuffle(iterator)
+	batch_loss = 0
+	for j in range(25):
+		i = iterator[j]
+		trial_data = data[i].view(1,-1)
+		yPred = model.forward(trial_data)
+		lossGrad, loss = lossClass.backward(yPred, torch.DoubleTensor([labels[i]]))
+		batch_loss += (loss)
+		model.clearGradParam()
+		for layer in model.Layers:
+			if layer.isTrainable:
+				layer.weight -= learningRate*layer.gradWeight
+				layer.bias -= learningRate*layer.gradBias
+	print(batch_loss)			
+	model.backward(trial_data, lossGrad)			
+
+print("\n\nLets check accuracy")
+
+count = 0
+
+for i in range(25):
+	trial_data = data[i].view(1,-1)
+	yPred = model.forward(trial_data)
+	count += (yPred.view(1,-1).max(dim=1)[1]==int(labels[i]))
+print(count)
